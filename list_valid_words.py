dbf37e0a4a1ea7a8a18863051215552a7b90c945
Andrew Morton <dev.private.user@gmail.com>
2019-12-26 18:49:58 -0500

    verified the words that have valid 2 letter words as their endings and saved to a file

--- list_valid_words.py
+++ list_valid_words.py
@@ -1,3 +1,6 @@
+from functools import partial
+
+
 def prepare_word_list(input_list):
 
     def open_list_path(file_path):
@@ -14,7 +17,7 @@ def prepare_word_list(input_list):
     return remove_newlines(open_list_path(input_list))
 
 
-def compare_word_endings(word, ending_distance, list_path):
+def compare_word_endings(ending_distance, list_path, word):
     letter_list = prepare_word_list(list_path)
 
     def bite_word(word, distance_from_end):
@@ -43,11 +46,23 @@ def compare_word_endings(word, ending_distance, list_path):
     return find_two_letter_ending_matches(word, ending_distance, letter_list)
 
 
-def main(english_list_path):
-    valid_list = prepare_word_list(english_list_path)
-    return valid_list
+def write_list_to_file(filename, item_list):
+    with open(filename, 'w') as filehandle:
+        filehandle.writelines("%s\n" % item for item in item_list)
 
 
-word_data_path = "./word_source/example.txt"
-# if the last 2 letters of the word are ALSO on the list of words,
-# then save that word in another data set
+def main():
+    words_data_path = "./word_source/words.txt"
+    two_letter_word_path = "./word_source/2_letter_words.txt"
+    valid_2_letter_file = "./valid_2_letter_endings.txt"
+
+    english_list = prepare_word_list(words_data_path)
+
+    filter_function = partial(
+            compare_word_endings,
+            [-2, None, None],
+            two_letter_word_path)
+
+    results = filter(filter_function, english_list)
+    write_list_to_file(valid_2_letter_file, results)
+    print(f"Wrote items to {valid_2_letter_file}")
